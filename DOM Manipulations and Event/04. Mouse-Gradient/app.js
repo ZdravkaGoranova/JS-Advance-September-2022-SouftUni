3cb2e2d5f851ab4326e4869f1c1e66e01235b381
Zdravka Goranova <106737347+ZdravkaGoranova@users.noreply.github.com>
2022-11-03 10:10:10 +0200

    Add files via upload

---
+++ DOM Manipulations and Event/04. Mouse-Gradient/app.js
@@ -0,0 +1,18 @@
+function attachGradientEvents() {
+    let gradient = document.getElementById("gradient");
+    gradient.addEventListener('mousemove', gradientMove);
+    gradient.addEventListener('mouseout', gradientOut);
+    debugger;
+
+    function gradientMove(event) {
+        let power = event.offsetX / (event.target.clientWidth - 1);
+       // let power = event.offsetX /  event.target.offsetWidth;//jund не приема .offsetWidth 
+        power = Math.trunc(power * 100);
+        document.getElementById('result').textContent = power + "%";
+    }
+
+    function gradientOut(event) {
+        document.getElementById("result").textContent = '';
+    }
+    
+}
