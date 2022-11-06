2019f1dab024334d360b1a2f4d66f0313a7f57e5
Zdravka Goranova <106737347+ZdravkaGoranova@users.noreply.github.com>
2022-11-07 01:23:08 +0200

    Add files via upload

---
+++ Exam Preparation and Exam/03. Book-selection/tests.js
@@ -0,0 +1,15 @@
+let { expect } = require("chai");
+let { bookSelection } = require("./solution");
+
+describe("Tests …", function() {
+    describe("TODO …", function() {
+
+  
+        it("TODO …", function() {
+            // TODO: …
+        });
+     });
+
+  
+     // TODO: …
+});
